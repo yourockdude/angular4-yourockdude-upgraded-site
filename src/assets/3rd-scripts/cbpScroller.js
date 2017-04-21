86ee7717ce4e8fa46adca230b36917446cb99941
Home <Home>
2017-04-21 16:47:25 +0300

    fixed mobile version(may be)

--- src/assets/3rd-scripts/cbpScroller.js
+++ src/assets/3rd-scripts/cbpScroller.js
@@ -8,8 +8,8 @@
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
-; (function (window) {
-
+;( function( window ) {
+	
 	'use strict';
 
 	var docElem = window.document.documentElement;
@@ -17,8 +17,8 @@
 	function getViewportH() {
 		var client = docElem['clientHeight'],
 			inner = window['innerHeight'];
-
-		if (client < inner)
+		
+		if( client < inner )
 			return inner;
 		else
 			return client;
@@ -29,24 +29,24 @@
 	}
 
 	// http://stackoverflow.com/a/5598797/989439
-	function getOffset(el) {
+	function getOffset( el ) {
 		var offsetTop = 0, offsetLeft = 0;
 		do {
-			if (!isNaN(el.offsetTop)) {
+			if ( !isNaN( el.offsetTop ) ) {
 				offsetTop += el.offsetTop;
 			}
-			if (!isNaN(el.offsetLeft)) {
+			if ( !isNaN( el.offsetLeft ) ) {
 				offsetLeft += el.offsetLeft;
 			}
-		} while (el = el.offsetParent)
+		} while( el = el.offsetParent )
 
 		return {
-			top: offsetTop,
-			left: offsetLeft
+			top : offsetTop,
+			left : offsetLeft
 		}
 	}
 
-	function inViewport(el, h) {
+	function inViewport( el, h ) {
 		var elH = el.offsetHeight,
 			scrolled = scrollY(),
 			viewed = scrolled + getViewportH(),
@@ -60,72 +60,74 @@
 		return (elTop + elH * h) <= viewed && (elBottom) >= scrolled;
 	}
 
-	function extend(a, b) {
-		for (var key in b) {
-			if (b.hasOwnProperty(key)) {
+	function extend( a, b ) {
+		for( var key in b ) { 
+			if( b.hasOwnProperty( key ) ) {
 				a[key] = b[key];
 			}
 		}
 		return a;
 	}
 
-	function cbpScroller(el, options) {
+	function cbpScroller( el, options ) {	
 		this.el = el;
-		this.options = extend(this.defaults, options);
+		this.options = extend( this.defaults, options );
 		this._init();
 	}
 
 	cbpScroller.prototype = {
-		defaults: {
+		defaults : {
 			// The viewportFactor defines how much of the appearing item has to be visible in order to trigger the animation
 			// if we'd use a value of 0, this would mean that it would add the animation class as soon as the item is in the viewport. 
 			// If we were to use the value of 1, the animation would only be triggered when we see all of the item in the viewport (100% of it)
-			viewportFactor: 0.2
+			viewportFactor : 0.2
 		},
-		_init: function () {
-			if (Modernizr.touch) return;
-			this.sections = Array.prototype.slice.call(this.el.querySelectorAll('.cbp-so-section'));
+		_init : function() {
+			// if( Modernizr.touch ) return;
+			// TODO Investigate this later
+			this.sections = Array.prototype.slice.call( this.el.querySelectorAll( '.cbp-so-section' ) );
 			this.didScroll = false;
 
 			var self = this;
 			// the sections already shown...
-			this.sections.forEach(function (el, i) {
-				if (!inViewport(el)) {
-					classie.add(el, 'cbp-so-init');
+			this.sections.forEach( function( el, i ) {
+				if( !inViewport( el ) ) {
+					classie.add( el, 'cbp-so-init' );
 				}
-			});
+			} );
 
-			var scrollHandler = function () {
-				if (!self.didScroll) {
-					self.didScroll = true;
-					setTimeout(function () { self._scrollPage(); }, 60);
-				}
-			},
-				resizeHandler = function () {
+			var scrollHandler = function() {
+					if( !self.didScroll ) {
+						self.didScroll = true;
+						setTimeout( function() { self._scrollPage(); }, 60 );
+					}
+				},
+				resizeHandler = function() {
 					function delayed() {
 						self._scrollPage();
 						self.resizeTimeout = null;
 					}
-					if (self.resizeTimeout) {
-						clearTimeout(self.resizeTimeout);
+					if ( self.resizeTimeout ) {
+						clearTimeout( self.resizeTimeout );
 					}
-					self.resizeTimeout = setTimeout(delayed, 200);
+					self.resizeTimeout = setTimeout( delayed, 200 );
 				};
 
-			window.addEventListener('scroll', scrollHandler, false);
-			window.addEventListener('resize', resizeHandler, false);
+			window.addEventListener( 'scroll', scrollHandler, false );
+			window.addEventListener( 'resize', resizeHandler, false );
 		},
-		_scrollPage: function () {
+		_scrollPage : function() {
 			var self = this;
-			this.sections.forEach(function (el, i) {
-				if (inViewport(el, self.options.viewportFactor)) {
-					classie.add(el, 'cbp-so-animate');
+
+			this.sections.forEach( function( el, i ) {
+				if( inViewport( el, self.options.viewportFactor ) ) {
+					classie.add( el, 'cbp-so-animate' );
 				}
 				else {
-					// this add class init if it doesn´t have it. This will ensure that the items initially in the viewport will also animate on scroll
-					classie.add(el, 'cbp-so-init');
-
-					classie.remove(el, 'cbp-so-animate');
+					// this add class init if it doesn't have it. This will ensure that the items initially in the viewport will also animate on scroll
+					classie.add( el, 'cbp-so-init' );
+					
+					classie.remove( el, 'cbp-so-animate' );
 				}
 			});
 			this.didScroll = false;
@@ -135,4 +137,4 @@
 	// add to global namespace
 	window.cbpScroller = cbpScroller;
 
-})(window);
+} )( window );
