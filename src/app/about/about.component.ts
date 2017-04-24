03863ef4b9b6e1a77ce8441e0b21cb7966f49de6
Home <Home>
2017-04-24 17:54:21 +0300

    added animation on about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,12 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { ContentService } from '../shared/services/content.service';
 import { Description } from '../shared/models/description';
 import { environment } from '../../environments/environment';
+import {
+    trigger,
+    state,
+    style,
+    animate,
+    transition,
+    keyframes,
+} from '@angular/animations';
 
 @Component({
     moduleId: module.id,
     selector: 'app-yourock-about',
     templateUrl: 'about.component.html',
-    providers: [ContentService]
+    providers: [ContentService],
+    animations: [
+        trigger('fadeIn', [
+            state('in', style({ opacity: 1 })),
+            state('void', style({ opacity: 0 })),
+            transition('* <=> *', animate('2000ms')),
+        ])
+    ]
 })
 
 export class AboutComponent implements OnInit {
